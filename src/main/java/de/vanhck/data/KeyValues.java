2e3bf8d8bc604ea8e1b4bf2b306e51926524cc31
Lotti <charlotte.proeller@gmx.de>
2017-06-23 22:04:36 +0200

    added some FileParser and Saver and some Data model information.

---
+++ src/main/java/de/vanhck/data/KeyValues.java
@@ -0,0 +1,10 @@
+package de.vanhck.data;
+
+/**
+ *
+ * Add the keys we currently support ( not nice, and can later be changed
+ * Created by Lotti on 6/23/2017.
+ */
+public enum KeyValues {
+    VALUE1, VALUE2
+}
