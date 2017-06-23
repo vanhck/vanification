4844b94988abab0d456654fdec709b25fe7ac061
Lotti <charlotte.proeller@gmx.de>
2017-06-23 20:23:38 +0200

    Changed project to spring-boot project and added root view

--- src/main/java/de/vanhck/RootUI.java
+++ src/main/java/de/vanhck/RootUI.java
@@ -6,6 +6,7 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
@@ -20,28 +21,10 @@ import com.vaadin.ui.VerticalLayout;
  * overridden to add component to the user interface and initialize non-component functionality.
  */
 @Theme("mytheme")
-public class MyUI extends UI {
-
+@SpringUI
+public class RootUI extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
-
-        Button button = new Button("Click Me");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
-        
-        layout.addComponents(name, button);
-        
-        setContent(layout);
-    }
-
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
+        setContent(new Label("Hello! I'm the root UI!"));
     }
 }
---
+++ src/main/java/de/vanhck/VanificationApplication.java
@@ -0,0 +1,12 @@
+package de.vanhck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanificationApplication.class, args);
+	}
+}
